d2c44bb419da2b0a55fbf38d392bd31884995645
augui <aguillon.ent@gmail.com>
2022-08-23 09:55:19 +0200

    initial commit

---
+++ src/main/java/com/example/birthdayapp/BirthDayAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.birthdayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthDayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthDayAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/birthdayapp/BirthDayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.birthdayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthDayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
