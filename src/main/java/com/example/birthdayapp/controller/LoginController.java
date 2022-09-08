d8f92e76c040efe98c5aed65687922d7bff8a4e6
Augustin <aguillon.ent@gmail.com>
2022-09-08 20:01:00 +0200

    [END]

--- src/main/java/com/example/birthdayapp/controller/LoginController.java
+++ src/main/java/com/example/birthdayapp/controller/LoginController.java
@@ -2,13 +2,10 @@ package com.example.birthdayapp.controller;
 
 import com.example.birthdayapp.models.Users;
 import com.example.birthdayapp.services.UsersService;
-import org.hibernate.annotations.common.util.impl.Log;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/login")
